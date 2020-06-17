a682fa2542c903631380971341eceae41f741d73
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-06-17 21:44:18 +0200

    change to 428lowpupatch container

--- myscript.sh
+++ myscript.sh
@@ -2,7 +2,14 @@
 
 cd /home/cmsusr
 
-set   -e
+set -e
+
+echo "Setting up CMSSSW_4_2_8_lowpupatch1"
+source /opt/cms/cmsset_default.sh
+scramv1 project CMSSW CMSSW_4_2_8_lowpupatch1
+cd CMSSW_4_2_8_lowpupatch1/src
+eval `scramv1 runtime -sh`
+echo "CMSSW_4_2_8_lowpupatch1 is at your service."
 
 pwd
 ls -l
