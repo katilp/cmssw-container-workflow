6b814d4b8c36f40f7365a840782f6a2a63bc3cb4
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-06-18 07:28:13 +0200

    change shared volume owner

--- commands.sh
+++ commands.sh
@@ -2,9 +2,14 @@
 
 pwd
 ls -l
+touch container_output.txt
 echo 'ls -l /home/cmsusr:'
 ls -l /home/cmsusr
-echo 'ls -l /usr:'
-ls -l /usr
-echo 'ls -l /home:'
-ls -l /home
+echo 'ls -l /home//runner/work/cmssw-container-workflow/cmssw-container-workflow:'
+ls -l /home/runner/work/cmssw-container-workflow/cmssw-container-workflow
+
+# copy output file to the shared volume, changing owner needed
+sudo chown -R cmsusr /home/runner/work/cmssw-container-workflow/cmssw-container-workflow/
+chmod 755 /home/runner/work/cmssw-container-workflow/cmssw-container-workflow/
+mkdir /home/runner/work/cmssw-container-workflow/cmssw-container-workflow/outputs
+cp container_output.txt /home/runner/work/cmssw-container-workflow/cmssw-container-workflow/outputs
