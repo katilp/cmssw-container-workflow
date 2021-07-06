38a0c2d33ccea12bec00f737458e6b467a58b68e
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2021-07-06 17:35:25 +0200

    Env test v1 (#3)

--- commands.sh
+++ commands.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 # echo pwd `pwd`: /home/cmsusr/CMSSW_5_3_32/src
 # echo $USER cmsusr
+echo pwd `pwd`
 sudo chown $USER /mnt/vol
 
-chmod +x /mnt/vol/ascript.sh
 echo Check if env variables persist:
 echo DUMMY_SET_IN_SCRIPT: $DUMMY_SET_IN_SCRIPT
 echo Source a script
@@ -11,3 +11,13 @@ source /mnt/vol/ascript.sh
 
 echo Script sourced
 echo DUMMY_SET_IN_SCRIPT: $DUMMY_SET_IN_SCRIPT
+
+
+echo git versions:
+which git
+git --version
+echo openssl versions:
+which openssl 
+openssl version || echo ignore exit code
+
+echo PATH before entrypoint $PATH
