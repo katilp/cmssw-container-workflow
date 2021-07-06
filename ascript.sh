60c3f9c6d8aaba7b76d345f53a8003218dd33f5a
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2021-07-06 19:17:14 +0200

    Update ascript.sh

--- ascript.sh
+++ ascript.sh
@@ -4,7 +4,7 @@ set  -e
 
 export DUMMY_SET_IN_SCRIPT=NEWVALUE
 
-echo Script set the variable DUMMY_SET_IN_SCRIPT to $DUMMY_SET_IN_SCRIPT  
+echo Script set the variable DUMMY_SET_IN_SCRIPT  to $DUMMY_SET_IN_SCRIPT  
 
 
 exec "$@"
