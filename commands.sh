efe354fd5ed21492f61bbe4ccdae9f0ffc7f58c1
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2022-07-27 14:45:24 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -11,8 +11,6 @@ pwd
 echo ls -l:
 ls -l
 
-sudo chown $USER /mnt/vol
-
 git clone -b odws2022-ttbaljets-prod https://github.com/cms-opendata-analyses/PhysObjectExtractorTool.git
 cd PhysObjectExtractorTool/PhysObjectExtractor
 scram b
@@ -21,6 +19,10 @@ cmsRun python/poet_cfg.py True > poet.out
 mkdir output
 cp poet.out ouput
 cp myoutput.root output
-mv poet.out /mnt/vol
-mv myoutput.root /mnt/vol/
+# if statement only needed for the docker workflow which does not use /mnt/vol
+if [ -d /mnt/vol ]; then
+  mv poet.out /mnt/vol
+  mv myoutput.root /mnt/vol/
+fi
+
 
