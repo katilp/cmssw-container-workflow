f183c4497e7c183808531ad632025745abce4ebb
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2022-11-13 21:18:32 +0100

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -11,6 +11,7 @@ pwd
 echo ls -l:
 ls -l
 
+
 git clone -b odws2022-ttbaljets-prod https://github.com/cms-opendata-analyses/PhysObjectExtractorTool.git
 cd PhysObjectExtractorTool/PhysObjectExtractor
 scram b
