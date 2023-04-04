9a5ef1cf889df4c65c79b953b4a3d860a94fb62f
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2023-04-04 18:48:50 +0200

    check if CMSSW_10_6_0 standalone starts

--- commands.sh
+++ commands.sh
@@ -12,19 +12,20 @@ echo ls -l:
 ls -l
 
 
-git clone -b odws2022-ttbaljets-prod https://github.com/cms-opendata-analyses/PhysObjectExtractorTool.git
-cd PhysObjectExtractorTool/PhysObjectExtractor
-scram b
+# git clone -b odws2022-ttbaljets-prod https://github.com/cms-opendata-analyses/PhysObjectExtractorTool.git
+# cd PhysObjectExtractorTool/PhysObjectExtractor
+# scram b
 
-cmsRun python/poet_cfg.py True > poet.out 2>&1
+# cmsRun python/poet_cfg.py True > poet.out 2>&1
 
 # /code/ouput and the if statement only needed for the docker workflow which does not use /mnt/vol
 mkdir /code/output
-cp poet.out /code/ouput
-cp myoutput.root /code/output
+# cp poet.out /code/ouput
+# cp myoutput.root /code/output
 if [ -d /mnt/vol ]; then
-  mv poet.out /mnt/vol
-  mv myoutput.root /mnt/vol/
+  touch /mnt/vol/some.out
+  # mv poet.out /mnt/vol
+  # mv myoutput.root /mnt/vol/
 fi
 
 
